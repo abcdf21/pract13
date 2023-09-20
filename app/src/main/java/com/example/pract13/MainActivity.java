eb980dc6b7801705c5ee4506836b91d4247dbbc1
stugl <isip_k.e.geraskin@mpt.ru>
2023-09-20 12:35:01 +0300

    llol

---
+++ app/src/main/java/com/example/pract13/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.pract13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText dateTxt, timeTxt;
+    ImageButton dateBtn, timeBtn;
+    Button applyBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dateTxt = findViewById(R.id.date_txt);
+        timeTxt = findViewById(R.id.time_txt);
+        dateBtn = findViewById(R.id.calendarBtn);
+        timeBtn = findViewById(R.id.timeBtn);
+        applyBtn = findViewById(R.id.applyBtn);
+
+        dateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int year = 2021;
+                int month = 11;
+                int day = 14;
+
+                DatePickerDialog.OnDateSetListener datePick = new DatePickerDialog.OnDateSetListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        dateTxt.setText(i2 + "-" + (i1 + 1) + "-" + i);
+                    }
+                };
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, datePick, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        timeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int hour = 13;
+                int minute = 23;
+                boolean is24Hours = true;
+
+                TimePickerDialog.OnTimeSetListener timePick = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        timeTxt.setText(i + ":" + i1);
+                    }
+                };
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, timePick, hour, minute, is24Hours);
+                timePickerDialog.show();
+            }
+        });
+
+        applyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Подтвреждение записи")
+                        .setIcon(R.drawable.ic_calendar)
+                        .setMessage("Вы подтверждаете запись?")
+                        .setPositiveButton("Подтвердить", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                Toast.makeText(MainActivity.this, "Ваша запись подтверждена", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        })
+                        .create();
+                builder.show();
+            }
+        });
+    }
+}
